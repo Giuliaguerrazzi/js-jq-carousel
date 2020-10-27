3003ce12719ca55d657bb7dbab69912a239a0708
Giulia <giulia.guerrazzi@live.it>
2020-10-27 16:35:10 +0100

    add next and prev

---
+++ main.js
@@ -0,0 +1,45 @@
+// slider di immagini
+$(document).ready( function() {
+
+// click del mouse
+ $('.next').click( function () {
+   direzione('next');
+ })
+
+ $('.prev').click( function () {
+   direzione('prev');
+ })
+
+})
+
+
+// funzioni
+ function direzione(direction){
+   var imageActive = $('.images img.active');
+   var iconActive = $('.nav i.active');
+
+   imageActive.removeClass('active');
+   iconActive.removeClass('active');
+
+   // next
+   if (direction === 'next') {
+     if (imageActive.hasClass('.last') === true) {
+       $('.images img.first').addClass('active');
+       $('.nav i.first').addClass('active');
+     }
+     else {
+       imageActive.next('img').addClass('active');
+       iconActive.next('i').addClass('active');
+     }
+   }
+  else if (direction === 'prev') {
+     if (imageActive.hasClass('.first') === true) {
+       $('.images img.last').addClass('active');
+       $('.nav i.last').addClass('active');
+     }
+     else {
+       imageActive.prev('img').addClass('active');
+       iconActive.prev('i').addClass('active');
+     }
+   }
+ }
